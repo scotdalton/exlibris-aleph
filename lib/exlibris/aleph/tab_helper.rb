4219db8de570588d97d0baf723ffe593cf52f759
barnabyalter <barnaby.alter@gmail.com>
2012-09-27 16:55:32 -0400

    Updated tab_helper to delete irrelevant sub libraries

--- lib/exlibris/aleph/tab_helper.rb
+++ lib/exlibris/aleph/tab_helper.rb
@@ -32,6 +32,15 @@ module Exlibris
       @@tab_path = nil
       @@yml_path = nil
       @@log_path = nil
+      @@irrelevant_sub_libraries = ["USR00", "HOME", "BOX", "ILLDT", "NYU51", "ALEPH", "USM50", 
+            "MED", "HYL", "HIL", "LAM", "LAW", "LIT", "MUS", "WID", "EXL", "CIRC", "HILR", "HIL01", 
+            "HYL01", "HYL02", "HYL03", "HYL04", "HYL05", "HYL06", "LAM01", "LAM02", "LAM03", "LAW01", 
+            "LAW02", "LAW03", "LIT01", "LIT02", "MED01", "MED02", "MUS01", "MUS02", "WID01", "WID02", 
+            "WID03", "WID04", "WID05", "U60WD", "U60HL", "U60LA", "U70WD", "CBAB", "BCU", "MBAZU", "USM51", 
+            "ELEC5", "GDOC5", "EDUC5", "LINC5", "RRLIN", "OU511", "OR512", "OR513", "OR514", "OR515", "U61ED", 
+            "U61EL", "U61LN", "S61GD", "USM53", "ELEC7", "GDOC7", "EDUC7", "LINC7", "USM54", "ELEC4", "USM55", 
+            "CUN50", "CLEC5", "CDOC5", "CDUC5", "CINC5", "UNI50", "NARCV", "NELEC", "NRLEC", "NGDOC", "NRDOC", 
+            "NEDUC", "NHLTH", "NLINC", "NLAW", "NMUSI", "NSCI", "NUPTN"]
       
       # Initialize TabHelper based on path to tabs, path to store yml configs,
       # path for log file, and the ADMs for the Aleph implementation
@@ -52,6 +61,11 @@ module Exlibris
             self.class.class_variable_get "#{class_variable}"
           end 
         end
+      end
+      
+      # Sets class variable of irrelevant sub libraries to be ignored when building sub_libraries YAML
+      def self.set_irrelevant_sub_libraries(irrelevant_sub_libraries)
+        @@irrelevant_sub_libraries = irrelevant_sub_libraries
       end
       
       # Refreshes the yml files that are used to parse the tables.
@@ -269,6 +283,8 @@ module Exlibris
             instance_variable_set("@#{key}".to_sym, tab)
           end
         end
+        # Delete irrelevant sub libraries from @sub_library
+        @sub_library.delete_if {|key,value| @@irrelevant_sub_libraries.include? key }
         @updated_at = Time.now()
       end
 
