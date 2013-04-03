9433ce4a39e170449fe72f3d7f0df635344332f0
Scot Dalton <scotdalton@gmail.com>
2013-04-03 09:49:24 -0400

    Add logging for False error.

--- test/tab_helper_test.rb
+++ test/tab_helper_test.rb
@@ -22,7 +22,7 @@ class TabHelperTest < ActiveSupport::TestCase
     assert_nothing_raised() { Exlibris::Aleph::TabHelper.send(:new) }
     assert_same(@helper, Exlibris::Aleph::TabHelper.instance)
   end
-  
+
   test "refresh" do
     @helper.instance_variable_set(:@refresh_time, lambda{1.second.ago})
     @helper.sub_libraries
@@ -35,27 +35,27 @@ class TabHelperTest < ActiveSupport::TestCase
     assert_not_equal(updated_at_1, @helper.updated_at)
     @helper.instance_variable_set(:@refresh_time, @helper.class.refresh_time)
   end
-  
+
   test "sub_library_text" do
     assert_equal("NYU Bobst", @helper.sub_library_text("BOBST"))
   end
-  
+
   test "sub_library_adm" do
     assert_equal("NYU50", @helper.sub_library_adm("BOBST"))
   end
-  
+
   test "item_pickup_locations" do
     assert_equal(
       ["BOBST", "NCOUR", "NIFA", "NISAW", "NREI", "NPOLY", "NYUAB", "NYUSE", "NYUSS"],
       @helper.item_pickup_locations({:adm_library_code => "nyu50", :sub_library_code => "BOBST", :bor_status => "51"}))
   end
-  
+
   test "collection_text" do
     assert_equal(
       "Main Collection",
       @helper.collection_text({:adm_library_code => "nyu50", :sub_library_code => "BOBST", :collection_code => "MAIN"}))
   end
-  
+
   test "item_web_text" do
     assert_equal(
       "Offsite Available",
@@ -64,18 +64,23 @@ class TabHelperTest < ActiveSupport::TestCase
       "Offsite Available",
       @helper.item_web_text({:adm_library_code => "nyu50", :sub_library_code => "BOBST", :item_process_status_code => "DP"}))
   end
-  
+
+  test "item_permissions" do
+    assert_equal("C", @helper.item_permissions(:adm_library_code => "nyu50",
+      :sub_library_code => "BOBST", :item_status_code => "01", :item_process_status_code => "DP")[:hold_request])
+  end
+
   test "sub_libraries" do
     assert_equal("BOBST", @helper.sub_libraries["BOBST"][:code])
     assert_equal("NYU Bobst", @helper.sub_libraries["BOBST"][:text])
     assert_equal("NYU50", @helper.sub_libraries["BOBST"][:library])
   end
-  
+
   test "patrons" do
     assert_equal("51", @helper.patrons["nyu50"]["51"][:code])
     assert_equal("NYU Administrator", @helper.patrons["nyu50"]["51"][:text])
   end
-  
+
   test "patron_permissions" do
     assert_equal("BOBST", @helper.patron_permissions["nyu50"]["BOBST"]["51"][:sub_library])
     assert_equal("51", @helper.patron_permissions["nyu50"]["BOBST"]["51"][:patron_status])
@@ -93,13 +98,13 @@ class TabHelperTest < ActiveSupport::TestCase
     assert_equal("N", @helper.patron_permissions["nyu50"]["BOBST"]["51"][:booking_ignore_closing_hours])
     assert_equal("N", @helper.patron_permissions["nyu50"]["BOBST"]["51"][:automatically_create_aleph_record])
   end
-  
+
   test "items" do
     assert_equal("ITEM-STATUS", @helper.items["nyu50"]["Billed as lost"][:code])
     assert_equal("Billed as lost", @helper.items["nyu50"]["Billed as lost"][:original_text])
     assert_equal("Request ILL", @helper.items["nyu50"]["Billed as lost"][:web_text])
   end
-  
+
   test "item_permissions_by_item_status" do
     assert_equal("BOBST", @helper.item_permissions_by_item_status["nyu50"]["BOBST"]["01"][:sub_library])
     assert_equal("01", @helper.item_permissions_by_item_status["nyu50"]["BOBST"]["01"][:item_status])
@@ -117,7 +122,7 @@ class TabHelperTest < ActiveSupport::TestCase
     assert_equal("00", @helper.item_permissions_by_item_status["nyu50"]["BOBST"]["01"][:reloaning_limit])
     assert_equal("A", @helper.item_permissions_by_item_status["nyu50"]["BOBST"]["01"][:booking_hours])
   end
-  
+
   test "item_permissions_by_item_process_status" do
     assert_equal("BOBST", @helper.item_permissions_by_item_process_status["nyu50"]["BOBST"]["AC"][:sub_library])
     assert_equal("AC", @helper.item_permissions_by_item_process_status["nyu50"]["BOBST"]["AC"][:item_process_status])
@@ -135,13 +140,13 @@ class TabHelperTest < ActiveSupport::TestCase
     assert_equal("00", @helper.item_permissions_by_item_process_status["nyu50"]["BOBST"]["AC"][:reloaning_limit])
     assert_equal("A", @helper.item_permissions_by_item_process_status["nyu50"]["BOBST"]["AC"][:booking_hours])
   end
-  
+
   test "collections" do
     assert_equal("BOBST", @helper.collections["nyu50"]["BOBST"]["MAIN"][:sub_library])
     assert_equal("MAIN", @helper.collections["nyu50"]["BOBST"]["MAIN"][:collection_code])
     assert_equal("Main Collection", @helper.collections["nyu50"]["BOBST"]["MAIN"][:text])
   end
-  
+
   test "pickup_locations" do
     assert_equal("BOBST", @helper.pickup_locations["nyu50"]["BOBST"]["##"]["DP"]["51"]["Y"][:sub_library])
     assert_equal("##", @helper.pickup_locations["nyu50"]["BOBST"]["##"]["DP"]["51"]["Y"][:item_status])
@@ -150,12 +155,12 @@ class TabHelperTest < ActiveSupport::TestCase
     assert_equal("Y", @helper.pickup_locations["nyu50"]["BOBST"]["##"]["DP"]["51"]["Y"][:availability_status])
     assert_equal(["BOBST", "NCOUR", "NIFA", "NISAW", "NREI", "NPOLY", "NYUAB", "NYUSE", "NYUSS"], @helper.pickup_locations["nyu50"]["BOBST"]["##"]["DP"]["51"]["Y"][:pickup_locations])
   end
-  
+
   test "pickup_locations on multiple lines" do
     assert_equal(["BOBST", "NCOUR", "NIFA","NISAW","NREI","NPOLY","NYUAB","NBERL",
       "NFLOR","NLOND","NPRAG","NWADC"], @helper.pickup_locations["nyu50"]["BOBST"]["##"]["##"]["89"]["#"][:pickup_locations])
   end
-  
+
   test "refresh_yml" do
     assert_nil(@helper.sub_libraries["NEW__"])
     sub_library_file = "#{File.dirname(__FILE__)}/mnt/aleph_tab/alephe/tab/tab_sub_library.eng"
