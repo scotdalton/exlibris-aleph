d15246e34c05943ccc47903e618fb2ba37db477d
Scot Dalton <scotdalton@gmail.com>
2014-04-24 12:50:15 -0400

    Add an example to the Config spec

--- spec/exlibris/aleph/config_spec.rb
+++ spec/exlibris/aleph/config_spec.rb
@@ -1,6 +1,16 @@
 module Exlibris
   module Aleph
     describe Config do
+      subject(:config) { Config }
+      describe '.admin_libraries' do
+        subject { Config.admin_libraries }
+        it { should be_an Array }
+        it 'should contain AdminLibraries' do
+          subject.each do |admin_library|
+            expect(admin_library).to be_an AdminLibrary
+          end
+        end
+      end
     end
   end
 end
