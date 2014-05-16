130384fc4d86c3b18a35e6acd79b06d1d518ad46
Scot Dalton <scotdalton@gmail.com>
2014-05-16 15:37:39 -0400

    Implement PickupLocation class

--- lib/exlibris/aleph/pickup_location.rb
+++ lib/exlibris/aleph/pickup_location.rb
@@ -1,6 +1,12 @@
 module Exlibris
   module Aleph
     class PickupLocation
+      attr_reader :code, :display
+
+      def initialize(code, display)
+        @code = code
+        @display = display
+      end
     end
   end
 end
