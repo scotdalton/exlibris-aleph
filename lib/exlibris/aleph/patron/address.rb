e6eae4a651e7d72a13bcb76d9d5e4b2136929132
Scot Dalton <scotdalton@gmail.com>
2014-05-16 15:33:24 -0400

    Expand address to include most fields and use the API

--- lib/exlibris/aleph/patron/address.rb
+++ lib/exlibris/aleph/patron/address.rb
@@ -3,15 +3,41 @@ module Exlibris
     class Patron
       class Address
         attr_reader :patron_id
+        (1..5).each do |n|
+          attr_reader "address#{n}".to_sym
+        end
+        (1..4).each do |n|
+          attr_reader "telephone#{n}".to_sym
+        end
+        attr_reader :zip, :sms_number, :want_sms, :email
 
         def initialize(patron_id)
           @patron_id = patron_id
+          (1..5).each do |n|
+            address_n = "address#{n}".to_sym
+            instance_variable_set("@#{address_n}", reader.send(address_n))
+          end
+          (1..4).each do |n|
+            telephone_n = "telephone#{n}".to_sym
+            instance_variable_set("@#{telephone_n}", reader.send(telephone_n))
+          end
+          @zip = reader.zip
+          @sms_number = reader.sms_number
+          @want_sms = reader.want_sms
+          @email = reader.email
         end
 
         private
-        def rest_patron_address
-          @rest_patron_address ||=
-            Rest::Patron::Address.new(patron_id)
+        def client
+          @client ||= API::Client::Patron::Address.new(patron_id)
+        end
+
+        def root
+          @root ||= client.root
+        end
+
+        def reader
+          @reader ||= API::Reader::Patron::Address.new(root)
         end
       end
     end
--- spec/exlibris/aleph/patron/address_spec.rb
+++ spec/exlibris/aleph/patron/address_spec.rb
@@ -8,6 +8,59 @@ module Exlibris
         it { should be_an Address }
         describe '#patron_id' do
           subject { address.patron_id }
+          it { should eq patron_id }
+        end
+        describe '#address1' do
+          subject { address.address1 }
+          it { should eql 'SURNAME,GIVEN NAME'}
+        end
+        describe '#address2' do
+          subject { address.address2 }
+          it { should eql '123 ADDRESS, #APT'}
+        end
+        describe '#address3' do
+          subject { address.address3 }
+          it { should eql 'BROOKLYN'}
+        end
+        describe '#address4' do
+          subject { address.address4 }
+          it { should eql 'NY'}
+        end
+        describe '#address5' do
+          subject { address.address5 }
+          it { should eql 'NY'}
+        end
+        describe '#telephone1' do
+          subject { address.telephone1 }
+          it { should be_nil }
+        end
+        describe '#telephone2' do
+          subject { address.telephone2 }
+          it { should be_nil }
+        end
+        describe '#telephone3' do
+          subject { address.telephone3 }
+          it { should be_nil }
+        end
+        describe '#telephone4' do
+          subject { address.telephone4 }
+          it { should be_nil }
+        end
+        describe '#zip' do
+          subject { address.zip }
+          it { should eq '11215' }
+        end
+        describe '#sms_number' do
+          subject { address.sms_number }
+          it { should be_nil }
+        end
+        describe '#want_sms' do
+          subject { address.want_sms }
+          it { should eq 'N' }
+        end
+        describe '#email' do
+          subject { address.email }
+          it { should eq 'bor_id@library.nyu.edu' }
         end
       end
     end
