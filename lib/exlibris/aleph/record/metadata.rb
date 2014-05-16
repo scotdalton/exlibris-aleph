4799e77444791595aeff4e86136eda9e4d8a3b5c
Scot Dalton <scotdalton@gmail.com>
2014-05-16 15:36:14 -0400

    Record metadata is just a Metadata

--- lib/exlibris/aleph/record/metadata.rb
+++ lib/exlibris/aleph/record/metadata.rb
@@ -2,15 +2,6 @@ module Exlibris
   module Aleph
     class Record
       class Metadata < Exlibris::Aleph::Metadata
-
-        private
-        def rest_record
-          @rest_record ||= Rest::Record.new(rest_id, { view: 'full' })
-        end
-
-        def rest_id
-          @rest_id ||= admin_library.code + id
-        end
       end
     end
   end
