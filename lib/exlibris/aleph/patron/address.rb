bec310573222bf7c2f95b6be8d7373489835da8c
Scot Dalton <scotdalton@gmail.com>
2014-06-24 10:59:17 -0400

    Lazy load the address API elements so the object doesn't make unnecessary HTTP calls

--- lib/exlibris/aleph/patron/address.rb
+++ lib/exlibris/aleph/patron/address.rb
@@ -3,28 +3,39 @@ module Exlibris
     class Patron
       class Address
         attr_reader :patron_id
-        (1..5).each do |n|
-          attr_reader "address#{n}".to_sym
-        end
-        (1..4).each do |n|
-          attr_reader "telephone#{n}".to_sym
-        end
-        attr_reader :zip, :sms_number, :want_sms, :email
 
         def initialize(patron_id)
           @patron_id = patron_id
-          (1..5).each do |n|
-            address_n = "address#{n}".to_sym
-            instance_variable_set("@#{address_n}", reader.send(address_n))
+        end
+
+        (1..5).each do |n|
+          address_n = "address#{n}".to_sym
+          define_method(address_n) do
+            eval("@#{address_n} ||= reader.send(address_n)")
           end
-          (1..4).each do |n|
-            telephone_n = "telephone#{n}".to_sym
-            instance_variable_set("@#{telephone_n}", reader.send(telephone_n))
+        end
+
+        (1..4).each do |n|
+          telephone_n = "telephone#{n}".to_sym
+          define_method(telephone_n) do
+            eval("@#{telephone_n} ||= reader.send(telephone_n)")
           end
-          @zip = reader.zip
-          @sms_number = reader.sms_number
-          @want_sms = reader.want_sms
-          @email = reader.email
+        end
+
+        def zip
+          @zip ||= reader.zip
+        end
+
+        def sms_number
+          @sms_number ||= reader.sms_number
+        end
+
+        def want_sms
+          @want_sms ||= reader.want_sms
+        end
+
+        def email
+          @email ||= reader.email
         end
 
         private
