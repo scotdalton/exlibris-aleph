ff0807fa33893ebb82c9b86ea5e7f7ff14f25981
Scot Dalton <scotdalton@gmail.com>
2014-06-24 11:00:39 -0400

    Lazy load the patron API elements so the object doesn't make unnecessary HTTP calls

--- lib/exlibris/aleph/patron.rb
+++ lib/exlibris/aleph/patron.rb
@@ -1,12 +1,18 @@
 module Exlibris
   module Aleph
     class Patron
-      attr_reader :id, :admin_library, :address
+      attr_reader :id
 
       def initialize(id)
         @id = id
-        @admin_library = AdminLibrary.new(reader.admin_library_code)
-        @address = Address.new(id)
+      end
+
+      def admin_library
+        @admin_library ||= AdminLibrary.new(reader.admin_library_code)
+      end
+
+      def address
+        @address ||= Address.new(id)
       end
 
       def record(record_id)
