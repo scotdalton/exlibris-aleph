47cb3ac2b0d11ccb24388b8e8245966a64572dfc
Scot Dalton <scotdalton@gmail.com>
2014-07-03 09:34:51 -0400

    Remove unnecessary parameters input argument from the Hold API client since the Hold creation call has been moved to it's own object.

--- lib/exlibris/aleph/api/client/patron/record/item/hold.rb
+++ lib/exlibris/aleph/api/client/patron/record/item/hold.rb
@@ -8,28 +8,16 @@ module Exlibris
               class Hold < Base
                 attr_reader :patron_id, :record_id, :item_id, :parameters
 
-                def initialize(patron_id, record_id, item_id, parameters=nil)
+                def initialize(patron_id, record_id, item_id)
                   @patron_id = patron_id
                   @record_id = record_id
                   @item_id = item_id
-                  unless parameters.nil?
-                    unless parameters.is_a?(Parameters)
-                      raise ArgumentError.new("Expecting #{parameters} to be a Parameters")
-                    end
-                    @parameters = parameters
-                    @request_method = :put
-                  end
                 end
 
                 protected
                 def path
                   @path ||=
                     "#{super}/patron/#{patron_id}/record/#{record_id}/items/#{item_id}/hold"
-                end
-
-                private
-                def put
-                  connection.put(path, "post_xml=#{parameters.to_xml}")
                 end
               end
             end
